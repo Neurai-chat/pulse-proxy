9791ab9fdba9269633147eb11abf47220a546746
Balazs <bali.kalmar@gmail.com>
2026-08-17 18:54:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post("/chat", async (req, res) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
+    
     },
     body: JSON.stringify(body)
   });
